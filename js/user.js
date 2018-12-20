ba0edcd32fb71266c279865f43113f4debf692f9
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2018-12-20 21:25:23 +0100

    View meetups and post questions specific meetup
    cotains meetup question form

---
+++ js/user.js
@@ -0,0 +1,5 @@
+document.getElementById('askQuestionButton').addEventListener('click', function displayQuestionForm() {
+    document.getElementById('postQuestionForm').style.display="block"
+}
+)
+
