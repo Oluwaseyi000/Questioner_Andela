82dfc9526e62d7b45e90f33c1f2d365ffb620422
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-02 19:40:51 +0100

    Get A Specific Meetup Record

---
+++ routes.js
@@ -0,0 +1,36 @@
+let express = require('express');
+let router = express.Router();
+let meetupController=require('./controller/Meetup');
+
+router.post('/meetups', meetupController.createMeetup);
+router.get('/meetups/:meetupId', meetupController.getASpecificMeetupRecord);
+
+
+router.route('*')
+   .get((req, res) => {
+      res.json({
+         status: 404,
+         message: 'Incorrect API endpoint;  Preceed your API endpoint with API please check your api URL (even for as little thing as spelling)'
+      });
+   })
+
+   .post((req, res) => {
+      res.json({
+         status: 404,
+         message: 'Incorrect API endpoint,  Preceed your API endpoint with API please check your api  URL(preceed your request url with api/v1)'
+      });
+   })
+   .put((req, res) => {
+      res.json({
+         status: 404,
+         message: 'Incorrect API endpoint,  Preceed your API endpoint with API please check your api URL (preceed your request url with api/v1)'
+      });
+   })
+   .delete((req, res) => {
+      res.json({
+         status: 404,
+         message: 'Incorrect API endpoint,  Preceed your API endpoint with API please check your api  URL(preceed your request url with api/v1)'
+      });
+   });
+
+module.exports = router;
