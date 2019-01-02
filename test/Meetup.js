d1c3762c0557c5a1bf1cd06af4cd3f10879a9047
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-02 21:49:10 +0100

    Get All Meetups Record GET /meetups

--- test/Meetup.js
+++ test/Meetup.js
@@ -1,36 +1,32 @@
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let assert = chai.assert;
-
 let server = require('../server');
 let Meetups = require('../model/Meetup');
 let meetupController = require('../controller/Meetup');
-let routes =require('../routes');
-
+let routes = require('../routes');
 
 chai.use(chaiHttp);
-describe('/NON-PERSISTENCE DATABASE', ()=>{
-   it('Assert non-persistence database using array', done=>{
+describe('/NON-PERSISTENCE DATABASE', () => {
+   it('Assert non-persistence database using array', done => {
       assert.isArray(Meetups);
       done();
    })
 })
 
-describe('/controller has createMeetup function', done=>{
-   it('Assert controller has a createMeetup function ', done=>{
-      assert.isFunction(meetupController.createMeetup);
-      done();
-  }) 
-})
-
 
 describe('/POST A MEETUP', () => {
+   describe('/controller has createMeetup function', done => {
+      it('Assert controller has a createMeetup function ', done => {
+         assert.isFunction(meetupController.createMeetup);
+         done();
+      })
+   })
    beforeEach(done => {
       Meetups = [];
       done();
    });
-
+   describe('create/post a new meetup', done => {
       it(' create/post a new meetup', done => {
          let newMeetup = {
             id: Date.now(),
@@ -43,7 +39,7 @@ describe('/POST A MEETUP', () => {
          chai.request(server)
             .post('/api/v1/meetups')
             .send(newMeetup)
-            .end((err, res) => {               
+            .end((err, res) => {
                assert.isObject(res.body);
                assert.equal(res.status, 200);
                assert.equal(res.body.status, 201);
@@ -53,19 +49,107 @@ describe('/POST A MEETUP', () => {
                   assert.isNotEmpty(res.body.data[i].location);
                   assert.isNotEmpty(res.body.data[i].topic);
                   assert.isNumber(res.body.data[i].id);
-                  assert.nestedProperty(res.body.data[i],'host');
-                  assert.nestedProperty(res.body.data[i],'details');
-                  assert.nestedProperty(res.body.data[i],'coverImage');
+                  assert.nestedProperty(res.body.data[i], 'host');
+                  assert.nestedProperty(res.body.data[i], 'details');
+                  assert.nestedProperty(res.body.data[i], 'coverImage');
                   assert.isNotEmpty(res.body.data[i].happeningOn);
                   assert.isArray(res.body.data[i].tag);
-                 
+
                }
             });
          done();
       })
-      afterEach(done => {
-         Meetups = [];
+   })
+
+   afterEach(done => {
+      Meetups = [];
+      done();
+   });
+
+})
+
+
+describe('/GET A SPECIFIC MEETUP', () => {
+
+   describe('/controller has getAllMeetups function', done => {
+      it('Assert controller has a getASpecificMeetupRecord function ', done => {
+         assert.isFunction(meetupController.getASpecificMeetupRecord);
          done();
-      });
+      })
+   })
+
+   describe('get a specific record in the database', done => {
+      it('get a specific record in the database', done => {
+         chai.request(server)
+            .get('/api/v1/meetups/:meetupId')
+            .end((err, res) => {
+
+               assert.isObject(res.body);
+               assert.equal(res.status, 200);
+               assert.isArray(res.body.data);
+
+               if (res.body.data.length === 0) {
+                  assert.equal(res.body.status, 204);
+                  assert.equal(res.body.message, 'Request successful but result contains no data, probably a case of non existence meetup id');
+
+               } else {
+                  assert.equal(res.body.data.length, 1);
+                  assert.equal(res.body.status, 200);
+                  assert.isNotEmpty(res.body.data[0].location);
+                  assert.isNotEmpty(res.body.data[0].topic);
+                  assert.isNumber(res.body.data[0].id);
+                  assert.nestedProperty(res.body.data[0], 'host');
+                  assert.nestedProperty(res.body.data[0], 'details');
+                  assert.nestedProperty(res.body.data[0], 'coverImage');
+                  assert.isNotEmpty(res.body.data[0].happeningOn);
+                  assert.isArray(res.body.data[0].tags);
+               }
+            });
+         done();
+      })
+   })
+
+})
+
+describe('/GET ALL MEETUPS ', () => {
+
+   describe('/controller has getAllMeetupsRecord function', done => {
+      it('Assert controller has a getAllMeetupsRecord function ', done => {
+         assert.isFunction(meetupController.getAllMeetupsRecord);
+         done();
+      })
+   })
+
+   describe('get all meetups record in the database', done => {
+      it('get all records in the database', done => {
+         chai.request(server)
+            .get('/api/v1/meetups')
+            .end((err, res) => {
+
+               assert.isObject(res.body);
+               assert.equal(res.status, 200);
+               assert.isArray(res.body.data);
+
+               if (res.body.data.length === 0) {
+                  assert.equal(res.body.status, 204);
+                  assert.equal(res.body.message, 'Request successful but result contains no data, probably no meetup records');
+
+               } else {
+                  for (let i = 0; i < res.body.data.length; i++) {
+                     assert.isNotEmpty(res.body.data[i].location);
+                     assert.isNotEmpty(res.body.data[i].topic);
+                     assert.isNumber(res.body.data[i].id);
+                     assert.nestedProperty(res.body.data[i], 'host');
+                     assert.nestedProperty(res.body.data[i], 'details');
+                     assert.nestedProperty(res.body.data[i], 'coverImage');
+                     assert.isNotEmpty(res.body.data[i].happeningOn);
+                     assert.isArray(res.body.data[i].tag);
    
+                  }
+               }
+            });
+         done();
+      })
+   })
+
 })
