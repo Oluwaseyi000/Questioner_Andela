21f4c91046fed9e3a1261e3c099dfb80fa552a5a
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-05 11:49:09 +0100

    Refactor UI into view directory moving UI into view using MVC approach

--- view/js/general.js
+++ view/js/general.js
@@ -1,7 +1,3 @@
-
-document.getElementById('currentTime').innerHTML = new Date().toLocaleDateString('en-GB', { hour: "numeric", 
-minute: "numeric"});
-
 let overlay = document.getElementById('loader-overlay')
 window.addEventListener('load', function(){
 overlay.style.display = 'none';
