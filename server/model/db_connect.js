a056ed9480552606c2aa02b6e54cee253ee0ebd7
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-18 08:12:19 +0100

    setting

--- server/model/db_connect.js
+++ server/model/db_connect.js
@@ -3,19 +3,13 @@ import {Pool, Client} from 'pg';
 
 dotenv.config();  
 const pool = new Pool({
-   connectionString: process.env.DATABASE_LOCAL,
+   connectionString: process.env.DATABASE_REMOTE
 });
 
-const pool2= new Pool({
-   user:'postgres',
-   host: 'localhost',
-   database:'questioner',
-   password:'a1b2c3d4e5',
-   port:5432
-});
-pool2.connect((err, client, done)=>{
+
+pool.connect((err, client, done)=>{
 if(err){console.log('error')}
 else {console.log('db connected')}
 })
 
-export default pool2;
+export default pool;
