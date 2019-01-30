7ff6fe56936ad7f2aba0ab126ac929c703e789a8
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-30 13:56:10 +0100

    consume API

--- server/controller/Question.js
+++ server/controller/Question.js
@@ -1,171 +1,153 @@
+import { exists } from 'fs';
 import Questions from '../model/Question';
 import Meetups from '../model/Meetup';
 import Authenticate from '../middleware/authorize';
 import pool from '../model/db_connect';
-import { exists } from 'fs';
 
 const confirmToken = Authenticate.confirmToken;
 class questionController {
-   /**
+  /**
     * Create A Question
-    * @param {object} req 
+    * @param {object} req
     * @param {object} res
-    * @returns {object} question object 
+    * @returns {object} question object
     */
-   static createQuestion(req, res) {
+  static createQuestion(req, res) {
+    confirmToken(req, res);
 
-      confirmToken(req, res);
+
+    const text = 'INSERT INTO questions(createdBy,meetupId, title, body, vote) VALUES($1, $2, $3,$4, $5) returning id';
 
 
-      const text = `INSERT INTO questions(createdBy,meetupId, title, body, vote) VALUES($1, $2, $3,$4, $5) returning id`;
+    const value = [
+      res.authData.userDetail.id,
+      req.body.meetupId,
+      req.body.title,
+      req.body.body,
+      0,
+    ];
 
-     
-      const value = [
-         res.authData.userDetail.id,
-         req.body.meetupId,
-         req.body.title,
-         req.body.body,
-         0
-      ]
+    pool.query(text, value)
+      .then(question => res.status(201).json({
+        status: 201,
+        message: 'Question successfully added',
+        data: [{
+          user: res.authData.userDetail.id,
+          meetup: req.body.meetupId,
+          title: req.body.title,
+          body: req.body.body,
+        }],
+      }));
 
-      pool.query(text, value)
-         .then(question => {
-            return res.status(201).json({
-               status: 201,
-               message: 'Question successfully added',
-               data: [{
-                  user: res.authData.userDetail.id,
-                  meetup: req.body.meetupId,
-                  title: req.body.title,
-                  body: req.body.body
-               }],
-            })
-         })
+    // }
+  }
 
-      // }
-   }
-
-   static upvote(req, res) {
-      /**
+  static upvote(req, res) {
+    /**
        * Vote question: increase or decrease the vote count
-       * @param {object} req 
+       * @param {object} req
        * @param {object} res
        * @returns {object} vote counts
        */
-         const text = 'SELECT * FROM questions WHERE id=$1';
-         const value = [req.params.questionId];
-         
-         pool.query(text, value)
-         .then(question=>{           
-            req.currentVote= question.rows[0].vote;
+    const text = 'SELECT * FROM questions WHERE id=$1';
+    const value = [req.params.questionId];
 
-            const text2 = `UPDATE questions SET vote=$1 WHERE id=$2 RETURNING *`;
-         
-         const value2 = [question.rows[0].vote+1,req.params.questionId];
-         pool.query(text2, value2)
-         
-         return res.status(200).json({
-            status:200,
-            data:[
-               {
-                  meetup: req.params.questionId,
-                  title: question.rows[0].title,
-            body:question.rows[0].body,
-            vote: question.rows[0].vote+1
-               }
-            ]
-            
-         })            
-         });
+    pool.query(text, value)
+      .then((question) => {
+        req.currentVote = question.rows[0].vote;
 
-       
+        const text2 = 'UPDATE questions SET vote=$1 WHERE id=$2 RETURNING *';
 
-      
+        const value2 = [question.rows[0].vote + 1, req.params.questionId];
+        pool.query(text2, value2);
 
+        return res.status(200).json({
+          status: 200,
+          data: [
+            {
+              meetup: req.params.questionId,
+              title: question.rows[0].title,
+              body: question.rows[0].body,
+              vote: question.rows[0].vote + 1,
+            },
+          ],
 
-   }
-
-   static downvote(req, res) {
-      /**
-       * Vote question: increase or decrease the vote count
-       * @param {object} req 
-       * @param {object} res
-       * @returns {object} vote counts
-       */
-       
-         
-         const text = 'SELECT * FROM questions WHERE id=$1';
-         const value = [req.params.questionId];
-         
-         pool.query(text, value)
-         .then(question=>{           
-            req.currentVote= question.rows[0].vote;
-
-            const text2 = `UPDATE questions SET vote=$1 WHERE id=$2 RETURNING *`;
-         
-         const value2 = [question.rows[0].vote-1,req.params.questionId];
-         pool.query(text2, value2)
-         
-         return res.status(200).json({
-            status:200,
-            data:[
-               {
-                  meetup: req.params.questionId,
-                  title: question.rows[0].title,
-            body:question.rows[0].body,
-            vote: question.rows[0].vote-1
-               }
-            ]
-            
-         })            
-         });
-
-       
-
-      
-
-
-   }
-   static addComment(req, res) {
-      confirmToken(req, res);
-      if (!req.body.questionId || !req.body.comment) {
-         return res.status(400).json({
-            status: 400,
-            error: 'Bad Request, please include meetup question Id and comment in your request as parameter'
-         })
-      } else {
-
-         const text = `INSERT INTO comments(userid, questionId, body) VALUES($1, $2, $3)returning  body  `;
-
-         const value = [ 
-            res.authData.userDetail.id, req.body.questionId, req.body.comment
-         ]; 
-        
-
-      pool.query(text, value)
-      .catch((error)=>{
-         return res.json({
-            status: 404,
-            error,
-            error2: 'question do not exist for the specified question id'}) 
+        });
       });
-      
-      const text2 = `SELECT id, title,body FROM questions WHERE id=$1` ;
-      const value2 = [req.body.questionId];
+  }
 
-      pool.query(text2, value2)
-       .then(question=>{
-          return res.status(201).json({
-            status: 201,
-            data:{
-               question: question.rows[0].id,
-               title: question.rows[0].title,
-               body: question.rows[0].body,
-               comment: req.body.comment
-            } 
-          })
-       });
-      }
-   }
+  static downvote(req, res) {
+    /**
+       * Vote question: increase or decrease the vote count
+       * @param {object} req
+       * @param {object} res
+       * @returns {object} vote counts
+       */
+
+
+    const text = 'SELECT * FROM questions WHERE id=$1';
+    const value = [req.params.questionId];
+
+    pool.query(text, value)
+      .then((question) => {
+        req.currentVote = question.rows[0].vote;
+
+        const text2 = 'UPDATE questions SET vote=$1 WHERE id=$2 RETURNING *';
+
+        const value2 = [question.rows[0].vote - 1, req.params.questionId];
+        pool.query(text2, value2);
+
+        return res.status(200).json({
+          status: 200,
+          data: [
+            {
+              meetup: req.params.questionId,
+              title: question.rows[0].title,
+              body: question.rows[0].body,
+              vote: question.rows[0].vote - 1,
+            },
+          ],
+
+        });
+      });
+  }
+
+  static addComment(req, res) {
+    confirmToken(req, res);
+    if (!req.body.questionId || !req.body.comment) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Bad Request, please include meetup question Id and comment in your request as parameter',
+      });
+    }
+
+    const text = 'INSERT INTO comments(userid, questionId, body) VALUES($1, $2, $3)returning  body  ';
+
+    const value = [
+      res.authData.userDetail.id, req.body.questionId, req.body.comment,
+    ];
+
+
+    pool.query(text, value)
+      .catch(error => res.json({
+        status: 404,
+        error,
+        error2: 'question do not exist for the specified question id' 
+}));
+
+    const text2 = 'SELECT id, title,body FROM questions WHERE id=$1';
+    const value2 = [req.body.questionId];
+
+    pool.query(text2, value2)
+      .then(question => res.status(201).json({
+        status: 201,
+        data: {
+          question: question.rows[0].id,
+          title: question.rows[0].title,
+          body: question.rows[0].body,
+          comment: req.body.comment,
+        },
+      }));
+  }
 }
 export default questionController;
--- server/middleware/Meetup.js
+++ server/middleware/Meetup.js
@@ -1,77 +1,77 @@
 import Joi from 'joi';
+
 class Meetup {
+  static createMeetup(req, res, next) {
+    const meetupSchema = Joi.object().keys({
+      topic: Joi.string().min(5).required(),
+      location: Joi.string().required(),
+      happeningOn: Joi.required(),
+    });
+    const {
+      error,
+      value,
+    } = Joi.validate({
+      topic: req.body.topic,
+      location: req.body.location,
+      happeningOn: req.body.happeningOn,
+    }, meetupSchema, {
+      abortEarly: false,
+    });
 
-   static createMeetup(req, res, next) {
-      const meetupSchema = Joi.object().keys({
-         topic: Joi.string().min(5).required(),
-         location: Joi.string().required(),
-         happeningOn: Joi.required(),
+    if (error) {
+      const errorMsgs = error.details.map(error => error.message);
+      return res.status(400).json({
+        status: 400,
+        error: errorMsgs,
       });
-      const {
-         error,
-         value
-      } = Joi.validate({
-         topic: req.body.topic,
-         location: req.body.location,
-         happeningOn: req.body.happeningOn
-      }, meetupSchema, {
-         abortEarly: false
-      });
+    }
+    return next();
+  }
 
-      if (error) {
-         const errorMsgs = error.details.map(error => error.message)
-         return res.status(400).json({
-            status: 400,
-            error: errorMsgs,
-         })
-      }
-      next();
-   }
+  static getASpecificMeetupRecord(req, res, next) {
+    const meetupSchema = Joi.object().keys({
+      meetupId: Joi.required(),
+    });
+    const {
+      error,
+      value,
+    } = Joi.validate({
+      meetupId: req.params.meetupId,
+    }, meetupSchema, {
+      abortEarly: false,
+    });
 
-   static getASpecificMeetupRecord(req, res, next) {
-      const meetupSchema = Joi.object().keys({
-         meetupId: Joi.required()
+    if (error) {
+      const errorMsgs = error.details.map(error => error.message);
+      return res.status(400).json({
+        status: 400,
+        error: errorMsgs,
       });
-      const {
-         error,
-         value
-      } = Joi.validate({
-         meetupId: req.params.meetupId,
-      }, meetupSchema, {
-         abortEarly: false
-      });
+    }
+    next();
+  }
 
-      if (error) {
-         const errorMsgs = error.details.map(error => error.message)
-         return res.status(400).json({
-            status: 400,
-            error: errorMsgs,
-         })
-      }
-      next();
-   }
+  static deleteMeetup(req, res, next) {
+    const meetupSchema = Joi.object().keys({
+      meetupId: Joi.required(),
+    });
+    const {
+      error,
+      value,
+    } = Joi.validate({
+      meetupId: req.params.meetupId,
+    }, meetupSchema, {
+      abortEarly: false,
+    });
 
-   static deleteMeetup(req, res, next) {
-      const meetupSchema = Joi.object().keys({
-         meetupId: Joi.required()
+    if (error) {
+      const errorMsgs = error.details.map(error => error.message);
+      return res.status(400).json({
+        status: 400,
+        error: errorMsgs,
       });
-      const {
-         error,
-         value
-      } = Joi.validate({
-         meetupId: req.params.meetupId,
-      }, meetupSchema, {
-         abortEarly: false
-      });
-
-      if (error) {
-         const errorMsgs = error.details.map(error => error.message)
-         return res.status(400).json({
-            status: 400,
-            error: errorMsgs,
-         })
-      }
-      next();
-   }
+    }
+    next();
+  }
 }
-   export default Meetup;
+export default Meetup;
