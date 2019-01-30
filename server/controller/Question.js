33bd7770bf13b002b3c720045af4de1bb81ff244
Adebajo Oluwaseyi <adebajo.oluwaseyi@gmail.com>
2019-01-30 13:56:10 +0100

    consume API

--- server/controller/Question.js
+++ server/controller/Question.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
+=======
+import { exists } from 'fs';
+import Questions from '../model/Question';
+import Meetups from '../model/Meetup';
+import Authenticate from '../middleware/authorize';
+>>>>>>> consume API
 import pool from '../model/db_connect';
 
 class questionController {
   /**
+<<<<<<< HEAD
    * Create A Question
    * @param {object} req
    * @param {object} res
@@ -10,6 +18,20 @@ class questionController {
   static createQuestion(req, res) {
     const text = 'INSERT INTO questions(createdBy,meetupId, title, body, vote) VALUES($1, $2, $3,$4, $5) returning id';
 
+=======
+    * Create A Question
+    * @param {object} req
+    * @param {object} res
+    * @returns {object} question object
+    */
+  static createQuestion(req, res) {
+    confirmToken(req, res);
+
+
+    const text = 'INSERT INTO questions(createdBy,meetupId, title, body, vote) VALUES($1, $2, $3,$4, $5) returning id';
+
+
+>>>>>>> consume API
     const value = [
       res.authData.userDetail.id,
       req.body.meetupId,
@@ -19,7 +41,11 @@ class questionController {
     ];
 
     pool.query(text, value)
+<<<<<<< HEAD
       .then(() => res.status(201).json({
+=======
+      .then(question => res.status(201).json({
+>>>>>>> consume API
         status: 201,
         message: 'Question successfully added',
         data: [{
@@ -29,15 +55,28 @@ class questionController {
           body: req.body.body,
         }],
       }));
+<<<<<<< HEAD
+=======
+
+    // }
+>>>>>>> consume API
   }
 
   static upvote(req, res) {
     /**
+<<<<<<< HEAD
      * Vote question: increase or decrease the vote count
      * @param {object} req
      * @param {object} res
      * @returns {object} vote counts
      */
+=======
+       * Vote question: increase or decrease the vote count
+       * @param {object} req
+       * @param {object} res
+       * @returns {object} vote counts
+       */
+>>>>>>> consume API
     const text = 'SELECT * FROM questions WHERE id=$1';
     const value = [req.params.questionId];
 
@@ -52,6 +91,7 @@ class questionController {
 
         return res.status(200).json({
           status: 200,
+<<<<<<< HEAD
           data: [{
             meetup: req.params.questionId,
             title: question.rows[0].title,
@@ -95,6 +135,93 @@ class questionController {
 
         });
       });
+=======
+          data: [
+            {
+              meetup: req.params.questionId,
+              title: question.rows[0].title,
+              body: question.rows[0].body,
+              vote: question.rows[0].vote + 1,
+            },
+          ],
+
+        });
+      });
+  }
+
+  static downvote(req, res) {
+    /**
+       * Vote question: increase or decrease the vote count
+       * @param {object} req
+       * @param {object} res
+       * @returns {object} vote counts
+       */
+
+
+    const text = 'SELECT * FROM questions WHERE id=$1';
+    const value = [req.params.questionId];
+
+    pool.query(text, value)
+      .then((question) => {
+        req.currentVote = question.rows[0].vote;
+
+        const text2 = 'UPDATE questions SET vote=$1 WHERE id=$2 RETURNING *';
+
+        const value2 = [question.rows[0].vote - 1, req.params.questionId];
+        pool.query(text2, value2);
+
+        return res.status(200).json({
+          status: 200,
+          data: [
+            {
+              meetup: req.params.questionId,
+              title: question.rows[0].title,
+              body: question.rows[0].body,
+              vote: question.rows[0].vote - 1,
+            },
+          ],
+
+        });
+      });
+  }
+
+  static addComment(req, res) {
+    confirmToken(req, res);
+    if (!req.body.questionId || !req.body.comment) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Bad Request, please include meetup question Id and comment in your request as parameter',
+      });
+    }
+
+    const text = 'INSERT INTO comments(userid, questionId, body) VALUES($1, $2, $3)returning  body  ';
+
+    const value = [
+      res.authData.userDetail.id, req.body.questionId, req.body.comment,
+    ];
+
+
+    pool.query(text, value)
+      .catch(error => res.json({
+        status: 404,
+        error,
+        error2: 'question do not exist for the specified question id' 
+}));
+
+    const text2 = 'SELECT id, title,body FROM questions WHERE id=$1';
+    const value2 = [req.body.questionId];
+
+    pool.query(text2, value2)
+      .then(question => res.status(201).json({
+        status: 201,
+        data: {
+          question: question.rows[0].id,
+          title: question.rows[0].title,
+          body: question.rows[0].body,
+          comment: req.body.comment,
+        },
+      }));
+>>>>>>> consume API
   }
 }
 export default questionController;
--- server/controller/User.js
+++ server/controller/User.js
@@ -1,12 +1,22 @@
 /* eslint-disable consistent-return */
 import bcrypt from 'bcrypt-nodejs';
 import jwt from 'jsonwebtoken';
+<<<<<<< HEAD
 import dotenv from 'dotenv';
 import pool from '../model/db_connect';
 
 dotenv.config();
 
 class userController {
+=======
+import pool from '../model/db_connect';
+import Authenticate from '../middleware/authorize';
+
+const confirmToken = Authenticate.confirmToken;
+
+class userController {
+
+>>>>>>> consume API
   /**
     * Create A user signup
     * @param {object} req
@@ -14,8 +24,14 @@ class userController {
     * @returns {object} user object
     */
 <<<<<<< HEAD
+<<<<<<< HEAD
   static userSignup(req, res) {
     const text = 'INSERT INTO users(firstname, lastName, email, phoneNumber, othername, registered, isadmin, password) VALUES($1, $2, $3,$4, $5, $6, $7, $8) returning id, firstname, lastname, isadmin';
+=======
+  static userSignup(req, res) {
+
+    const text = 'INSERT INTO users(firstname, lastName, email, phoneNumber, othername, registered, isadmin, password) VALUES($1, $2, $3,$4, $5, $6, $7, $8) returning id, email, firstname, lastname, isadmin';
+>>>>>>> consume API
 
     const value = [
       req.body.firstname,
@@ -33,9 +49,15 @@ class userController {
         const userDetail = user.rows[0];
         jwt.sign({
           userDetail,
+<<<<<<< HEAD
         }, process.env.SECRET, (err, token) => {
           if (err) {
             res.json({ err });
+=======
+        }, 'secretkey', (err, token) => {
+          if (err) {
+            console.log(err);
+>>>>>>> consume API
           } else {
             return res.status(201).json({
               status: 201,
@@ -43,6 +65,7 @@ class userController {
               data: [{
                 token,
                 user: {
+<<<<<<< HEAD
                   firstname: userDetail.firstname,
                   lastname: userDetail.lastname,
                   email: userDetail.email,
@@ -87,28 +110,35 @@ class userController {
                            isadmin: user.rows[0].isadmin
                         },
                      }]
+=======
+                  userId: userDetail.id,
+                  firstname: user.rows[0].firstname,
+                  lastname: user.rows[0].lastname,
+                  email: user.rows[0].email,
+                  isadmin: user.rows[0].isadmin,
+                },
+              }],
+>>>>>>> consume API
 
 
-                  })
-               }
-            })
+            });
+          }
+        });
 
-         })
-         .catch((err) => {
-            return res.status(400).json({
-               status: 400,
-               message: err
-            })
-
-         })
+      })
+      .catch(err => res.status(400).json({
+        status: 400,
+        message: err,
+      }));
 
 
-   }
+  }
 
-   /**
-    * Login user 
-    * @param {object} req 
+  /**
+    * Login user
+    * @param {object} req
     * @param {object} res
+<<<<<<< HEAD
     * @returns {object} user object 
     */
    static userLogin(req, res) {
@@ -186,6 +216,15 @@ class userController {
   static userLogin(req, res) {
     const text2 = 'SELECT id,password,firstname,lastname, email, isadmin FROM users WHERE email=$1';
 
+=======
+    * @returns {object} user object
+    */
+  static userLogin(req, res) {
+
+    const text2 = 'SELECT id,password,firstname,lastname, email, isadmin FROM users WHERE email=$1';
+
+
+>>>>>>> consume API
     const value = [
       req.body.email,
     ];
@@ -194,6 +233,7 @@ class userController {
       .then(
         (user) => {
           if (user.rows.length > 0) {
+<<<<<<< HEAD
             const userDetail = {
               id: user.rows[0].id,
               firstname: user.rows[0].firstname,
@@ -274,33 +314,50 @@ class userController {
       }));
   }
 =======
+=======
+>>>>>>> consume API
 
-   static createRsvps(req, res) {
-      confirmToken(req, res);
-      if (!req.body.status || !req.params.meetupId) {
-         return res.status(400).json({
-            status: 400,
-            error: 'Bad request error, missing required data. Note: status is require'
-         })
-      } else {
-         const text = `INSERT INTO rsvps(userId, meetupId, response) VALUES($1, $2, $3) RETURNING response`;
+            const userDetail = user.rows[0];
+            bcrypt.compare(req.body.password, user.rows[0].password, (err, authPwd) => {
 
-         const value = [
-            res.authData.userDetail.id,
-            req.params.meetupId,
-            req.body.status
-         ]
-         pool.query(text, value)
-            .catch((error) => {
-               return res.status(409).json({
-                     status: 409,
-                     error2: 'RSVP already exist for user'
+              if (authPwd) {
+                jwt.sign({
+                  userDetail,
+                }, 'secretkey', (err, token) => {
+                  if (err) {} else {
+                    return res.status(200).json({
+                      status: 200,
+                      message: 'User logged in successfully',
+                      data: [{
+                        token,
+                        user: {
+                          userId: userDetail.id,
+                          firstname: user.rows[0].firstname,
+                          lastname: user.rows[0].lastname,
+                          email: user.rows[0].email,
+                          adminStatus: user.rows[0].isadmin,
+                        },
+                      }],
+                    });
                   }
-
-               )
+                });
+              } else {
+                return res.status(401).json({
+                  status: 401,
+                  error: 'wrong login credentials',
+                });
+              }
             });
 
+          } else {
+            return res.status(401).json({
+              status: 401,
+              error: 'wrong login credentials',
+            });
+          }
 
+
+<<<<<<< HEAD
          const text3 = `SELECT id, topic FROM meetups WHERE id=$1`;
          const value3 = [req.params.meetupId];
 
@@ -363,5 +420,105 @@ class userController {
 
    }
 >>>>>>> consuming
+=======
+
+
+        },
+      )
+      .catch(err => res.status(400).json({
+        status: 400,
+        ER: 'error',
+        message: err,
+      }));
+
+
+  }
+
+
+  /**
+    * Create A Rsvp
+    * @param {object} req
+    * @param {object} res
+    * @returns {object} rsvp object
+    */
+
+  static createRsvps(req, res) {
+    confirmToken(req, res);
+    if (!req.body.status || !req.params.meetupId) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Bad request error, missing required data. Note: status is require',
+      });
+    }
+    const text = 'INSERT INTO rsvps(userId, meetupId, response) VALUES($1, $2, $3) RETURNING response';
+
+    const value = [
+      res.authData.userDetail.id,
+      req.params.meetupId,
+      req.body.status,
+    ];
+    pool.query(text, value)
+      .catch(() => res.status(409).json({
+        status: 409,
+        error2: 'RSVP already exist for user',
+      },
+
+      ));
+
+
+    const text3 = 'SELECT id, topic FROM meetups WHERE id=$1';
+    const value3 = [req.params.meetupId];
+
+    pool.query(text3, value3)
+      .then(meetup => res.status(201).json({
+        status: 201,
+        message: 'RSVP successfully created',
+        data: [{
+          meetup: meetup.rows[0].id,
+          topic: meetup.rows[0].topic,
+          status: req.body.status,
+        }],
+      }))
+      .catch(() => res.status(404).json({
+        error: 'meetup  do not exit',
+      }));
+
+
+  }
+
+  static resetPassword(req, res) {
+
+    confirmToken(req, res);
+
+    if (!req.body.userId || !req.body.newPwd) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Bad Request, please include user Id and new password in your request as parameter',
+      });
+    }
+
+    const text = 'UPDATE users SET password=$1 where id=$2 RETURNING firstname, email';
+
+    const value = [
+      bcrypt.hashSync(req.body.newPwd, bcrypt.genSaltSync(10)),
+      req.body.userId,
+    ];
+
+    pool.query(text, value)
+      .then(user => res.status(200).json({
+        status: 200,
+        message: 'password successfully changed',
+        data: {
+          firstname: user.rows[0].firstname,
+          email: user.rows[0].email,
+        },
+      }))
+      .catch(() => res.status(400).json({
+        error: 'no user found',
+      }));
+
+
+  }
+>>>>>>> consume API
 }
 export default userController;
